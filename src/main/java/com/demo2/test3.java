9c8469c664aca4bf10eb1a91220c55d50492d7bc
duan <a15949687262@163.com>
2023-02-20 17:33:03 +0800

    hot-fix third commit

--- src/main/java/com/demo2/test3.java
+++ src/main/java/com/demo2/test3.java
@@ -3,5 +3,6 @@ package com.demo2;
 public class test3 {
     public static void main(String[] args) {
         System.out.println("iiiiiiiiiiiiii");
+        System.out.println("9999999999");
     }
 }
